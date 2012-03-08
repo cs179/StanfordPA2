81694209bea0017d703577c7d20cfd8074fd3b40
Nicholas Stanford <npstanford@gmail.com>
2012-03-07 21:11:59 -0500

    fixed another bug

--- common.php
+++ common.php
@@ -2,8 +2,8 @@
 	//let's define constants here
 	define("DB_USER","cs179");
 	define("DB_PASS","cs179pa2");
-	define("DB_HOST","localhost");
-	//define("DB_HOST","mysql.cs179.org");
+	//define("DB_HOST","localhost");
+	define("DB_HOST","mysql.cs179.org");
 	define("DB_NAME","cs179pa2");
 
 	//create a DB connection
